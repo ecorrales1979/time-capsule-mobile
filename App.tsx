bca649d85c2b63b271df82ffde2ea1eb2e9d4c88
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-17 01:46:55 -0300

    feat: add page contents

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, Text } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { styled } from 'nativewind'
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
@@ -6,6 +6,7 @@ import { useFonts, BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/spacetime_logo.svg'
 
 const StyledStripes = styled(Stripes)
 
@@ -21,11 +22,35 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
       // imageStyle={{ position: 'absolute', left: '-200%' }}
     >
       <StyledStripes className="absolute right-2" />
-      <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            começar a cadastrar
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
