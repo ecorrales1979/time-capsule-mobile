fd912d5bb78afbba2613fb8dd87feca798c39d96
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-17 01:29:51 -0300

    feat: add bg and stripes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import { ImageBackground, Text } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { styled } from 'nativewind'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { useFonts, BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -17,9 +19,14 @@ export default function App() {
   if (!hasLoadedFonts) return null
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      // imageStyle={{ position: 'absolute', left: '-200%' }}
+    >
+      <StyledStripes className="absolute right-2" />
       <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
