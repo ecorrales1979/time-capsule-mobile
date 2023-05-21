854919c92998c9179e712d71fc755e6580a70ba1
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-21 19:49:21 -0300

    feat: create layout of memories list

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,6 +1,6 @@
 import { Link, useRouter } from 'expo-router'
 import React from 'react'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import * as SecureStore from 'expo-secure-store'
@@ -18,10 +18,10 @@ export default function Memories() {
 
   return (
     <ScrollView
-      className="my-4 flex-1 px-8"
+      className="my-4 flex-1"
       contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
-      <View className="flex-row items-center justify-between">
+      <View className="flex-row items-center justify-between px-8">
         <Logo />
 
         <View className="flex-row gap-2">
@@ -40,7 +40,42 @@ export default function Memories() {
         </View>
       </View>
 
-      <Text className="text-gray-200">Memorias</Text>
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-50" />
+            <Text className="font-body text-xs text-gray-100">
+              12 abril 2020
+            </Text>
+          </View>
+          <View className="space-y-4 px-8">
+            <Image
+              source={{
+                uri: 'http://192.168.1.2:3333/download/2ffd772e-724c-4771-a56a-465cd7fe66de.jpg',
+              }}
+              alt=""
+              className="aspect-video w-full rounded-lg"
+            />
+            <Text className="font-body text-base leading-relaxed text-gray-100">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem est quibusdam nemo. Ullam harum accusantium eligendi
+              similique suscipit amet aliquam nemo perferendis beatae, quo
+              maiores dolore dolorum a, doloremque recusandae.
+            </Text>
+            <Link href={`memories/id`} asChild>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                className="block flex-row items-center gap-1.5"
+              >
+                <Text className="font-body text-sm text-gray-200">
+                  Ler mais
+                </Text>
+                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+      </View>
     </ScrollView>
   )
 }
