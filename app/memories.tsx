17e03e2268b5e3dce08c28c2ec1a591aa15d7794
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2023-05-21 20:08:50 -0300

    feat: load memories from api

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,20 +1,46 @@
+import { useEffect, useState } from 'react'
 import { Link, useRouter } from 'expo-router'
-import React from 'react'
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import * as SecureStore from 'expo-secure-store'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
 
 import Logo from '../src/assets/spacetime_logo.svg'
+import { api } from '../src/lib/api'
+
+interface MemoryDTO {
+  id: string
+  coverUrl: string
+  content: string
+  isPublic: boolean
+  createdAt: string
+}
 
 export default function Memories() {
   const { bottom, top } = useSafeAreaInsets()
   const router = useRouter()
+  const [memories, setMemories] = useState<MemoryDTO[]>([])
 
   const signOut = async () => {
     await SecureStore.deleteItemAsync('spacetime-token')
     router.push('/')
   }
+
+  const loadMemories = async () => {
+    const token = await SecureStore.getItemAsync('spacetime-token')
+    const getMemoriesResponse = await api.get<MemoryDTO[]>('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    setMemories(getMemoriesResponse.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
 
   return (
     <ScrollView
@@ -41,40 +67,41 @@ export default function Memories() {
       </View>
 
       <View className="mt-6 space-y-10">
-        <View className="space-y-4">
-          <View className="flex-row items-center gap-2">
-            <View className="h-px w-5 bg-gray-50" />
-            <Text className="font-body text-xs text-gray-100">
-              12 abril 2020
-            </Text>
+        {memories.map((memory) => (
+          <View key={`memory_${memory.id}`} className="space-y-4">
+            <View className="flex-row items-center gap-2">
+              <View className="h-px w-5 bg-gray-50" />
+              <Text className="font-body text-xs text-gray-100">
+                {dayjs(memory.createdAt)
+                  .locale(ptBr)
+                  .format('D[ de ]MMMM[, ]YYYY')}
+              </Text>
+            </View>
+            <View className="space-y-4 px-8">
+              <Image
+                source={{
+                  uri: memory.coverUrl,
+                }}
+                alt=""
+                className="aspect-video w-full rounded-lg"
+              />
+              <Text className="font-body text-base leading-relaxed text-gray-100">
+                {memory.content}
+              </Text>
+              <Link href={`memories/${memory.id}`} asChild>
+                <TouchableOpacity
+                  activeOpacity={0.7}
+                  className="block flex-row items-center gap-1.5"
+                >
+                  <Text className="font-body text-sm text-gray-200">
+                    Ler mais
+                  </Text>
+                  <Icon name="arrow-right" size={16} color="#9e9ea0" />
+                </TouchableOpacity>
+              </Link>
+            </View>
           </View>
-          <View className="space-y-4 px-8">
-            <Image
-              source={{
-                uri: 'http://192.168.1.2:3333/download/2ffd772e-724c-4771-a56a-465cd7fe66de.jpg',
-              }}
-              alt=""
-              className="aspect-video w-full rounded-lg"
-            />
-            <Text className="font-body text-base leading-relaxed text-gray-100">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatem est quibusdam nemo. Ullam harum accusantium eligendi
-              similique suscipit amet aliquam nemo perferendis beatae, quo
-              maiores dolore dolorum a, doloremque recusandae.
-            </Text>
-            <Link href={`memories/id`} asChild>
-              <TouchableOpacity
-                activeOpacity={0.7}
-                className="block flex-row items-center gap-1.5"
-              >
-                <Text className="font-body text-sm text-gray-200">
-                  Ler mais
-                </Text>
-                <Icon name="arrow-right" size={16} color="#9e9ea0" />
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </View>
+        ))}
       </View>
     </ScrollView>
   )
